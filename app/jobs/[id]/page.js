c7adf72a23e09ef9f7cf2e1d679798e38cbd0fc6
gayedinc <gayedinc190@gmail.com>
2025-06-08 16:00:49 +0300

    favicon updated

--- app/jobs/[id]/page.js
+++ app/jobs/[id]/page.js
@@ -1,11 +1,12 @@
-import data from '@/public/data.json';
-import JobDetails from '@/app/components/JobDetails';
+import data from "@/public/data.json";
 
-export default function JobDetailsPage({ params }) {
+export default async function JobDetailsPage({ params }) {
   const jobId = parseInt(params.id);
   const job = data.find((j) => j.id === jobId);
 
   if (!job) return <p>Job not found</p>;
+
+  const JobDetails = (await import("@/app/components/JobDetails")).default;
 
   return (
     <div className="container">
--- app/layout.js
+++ app/layout.js
@@ -4,15 +4,18 @@ import Header from './components/Header';
 export const metadata = {
   title: 'DevJobs Web App',
   description: 'Find your next developer job',
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
-    <>
-      <Header />
-      <html lang="en">
-        <body>{children}</body>
-      </html>
-    </>
+    <html lang="en">
+      <body>
+        <Header />
+        {children}
+      </body>
+    </html>
   );
 }
